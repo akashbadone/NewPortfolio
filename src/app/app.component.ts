40665910b247cb2f61518b8b2203aaf6ba6bf217
Akash Badone <akashbadone0@gmail.com>
2024-12-12 22:03:29 +0530

    thoda thoda chnage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'andresjosehr-portfolio';
+  title = 'akashbadone';
   
   constructor(
     private titleService: Title,
@@ -27,11 +27,11 @@ export class AppComponent implements OnInit{
     this.languageService.initLanguage()
 
 
-    this.titleService.setTitle( "José Andrés | Frontend Developer" );
+    this.titleService.setTitle( "Akash Badone | IT Analyst" );
 
     this.metaService.addTags([
-      {name: 'keywords', content: 'Frontend, software, developer'},
-      {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
+      {name: 'keywords', content: 'dot net, software, developer'},
+      {name: 'description', content: 'IT Analyst at MPOnline | Ex-MPSEDC | Ex-VYAPAM'},
     ]);
     
     
